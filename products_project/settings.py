e323a9a19830a3192a20e79836e56896e6fe953a
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-15 12:56:59 -0800

    Created Product for Products all + Registeres Products and Rest Framework in Project Settings Installed apps Settings

--- products_project/settings.py
+++ products_project/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lg5zc+uee=dd4xyl(x_zb4w*!yq1!eesl(loag^k7ihq_nby9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -69,20 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'products_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projects_database',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '1John4:4'
-    }
-}
 
 
 # Password validation
