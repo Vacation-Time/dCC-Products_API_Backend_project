0956f63efd2d8bf25ae3b7cfc4db65ba67d7ef6d
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-15 12:06:56 -0800

    Created Path to mysql Database + Updated local_settings File

--- products_project/settings.py
+++ products_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from ast import Import
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'products_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projects_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1John4:4'
     }
 }
 
@@ -121,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass
