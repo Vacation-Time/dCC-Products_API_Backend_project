bdde720ffe454d188c4872063f4f1cdbb4085909
The Luca <80943931+Vacation-Time@users.noreply.github.com>
2022-02-16 09:19:46 -0800

    Created New View Function For Getting Products By ID + Added New URLS Path For Product Detail View Function

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
+import re
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
+from products import serializers
 
 
 @api_view(['GET', 'POST'])
@@ -16,3 +18,13 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def products_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
